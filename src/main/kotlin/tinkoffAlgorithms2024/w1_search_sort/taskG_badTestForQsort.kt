eb20a0b4038a84c4c24b148ff186fb9cfdc32d24
MaffyMaths <matvey33.baryshev@mail.ru>
2024-02-29 13:07:23 +0300

    tinkoffAlgorithms2024: add week 1 bad qsort test

--- src/main/kotlin/tinkoffAlgorithms2024/w1_search_sort/taskG_badTestForQsort.kt
+++ src/main/kotlin/tinkoffAlgorithms2024/w1_search_sort/taskG_badTestForQsort.kt
@@ -2,7 +2,13 @@ package tinkoffAlgorithms2024.w1_search_sort
 
 fun main() {
     val n = readln().toInt()
+    val ans = IntArray(n) { it + 1 }
 
-    for (i in (n - n % 2) downTo 1 step 2) print("$i ")
-    for (i in 1..n step 2) print("$i ")
+    for (i in 2 until n) {
+        ans[i] += ans[i / 2]
+        ans[i / 2] = ans[i] - ans[i / 2]
+        ans[i] -= ans[i / 2]
+    }
+
+    for (i in ans) print("$i ")
 }
