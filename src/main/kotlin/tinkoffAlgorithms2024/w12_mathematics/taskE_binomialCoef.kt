1315606d76b9b949a58ecfbb08ac940490351c20
MaffyMaths <matvey33.baryshev@mail.ru>
2024-05-10 16:55:35 +0300

    tinkoffAlgorithms2024: week 12 task E

---
+++ src/main/kotlin/tinkoffAlgorithms2024/w12_mathematics/taskE_binomialCoef.kt
@@ -0,0 +1,18 @@
+package tinkoffAlgorithms2024.w12_mathematics
+
+/*
+mod = 1_000_000_007
+
+n, k = map(int, input().split())
+
+factorial = [1]
+for i in range(1, n + 1):
+    factorial.append((factorial[-1] * i) % mod)
+
+print((factorial[n] * pow(factorial[k], mod - 2, mod) * pow(factorial[n - k], mod - 2, mod)) % mod)
+
+ */
+
+fun main() {
+    println("Python solution")
+}
